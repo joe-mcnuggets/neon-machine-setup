b25af48e5f54b157819f86acbca7571e28c63674
Artemy Gavrelyuk <d213k@hotmail.com>
2019-05-21 16:01:41 +0300

    Add languages

--- installer.sh
+++ installer.sh
@@ -59,3 +59,35 @@ git clone https://github.com/jwiegley/use-package.git
 echo -e "${PURPLE}Installing zsh${NC}"
 sudo apt-get install zsh
 echo -e "${PURPLE}Run chsh -s $(which zsh) to set zsh as default${NC}" 
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# RVM
+echo -e "${PURPLE}Installing RVM${NC}"
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
+source /home/parrot/.rvm/scripts/rvm
+echo -e "${PURPLE}Installing Ruby 2.6.3${NC}"
+rvm install 2.6.3
+rvm --default use 2.6.3
+ruby -v
+
+# NodeJS
+echo -e "${PURPLE}Installing NodeJS${NC}"
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+node -v
+
+echo -e "${PURPLE}Installing Yarn${NC}"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
+echo -e "${PURPLE}Installing Go${NC}"
+cd ~
+# TODO: use variables
+wget https://dl.google.com/go/go1.12.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.12.5.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go -v
+# TODO: automate this
+echo "Add export PATH=$PATH:/usr/local/go/bin to your ./.profile"
